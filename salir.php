923661aa5975de85d132deaa169b559efab5782a
miluska <milu_nati_123@hotmail.com>
2014-05-06 00:13:09 -0500

    modificacion archivo salir.php

--- salir.php
+++ salir.php
@@ -1,6 +1,6 @@
 <?php
     require_once "conf/conf.ini.php";
-    //
+    //SALIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIR
     session_destroy();
     
     unset($_SESSION["login"]);
